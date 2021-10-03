a708f447caadb9d39dc1018b896c54c63fa99b66
Jasmin Jasani <jjasani@users.noreply.github.com>
2021-10-03 10:24:00 +0530

    Update ExampleInstrumentedTest.java
    Toast Added

--- app/src/androidTest/java/com/pcs/pcsvernier_firebase/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pcs/pcsvernier_firebase/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.pcs.pcsvernier_firebase", appContext.getPackageName());
+        Toast.makeText(getApplicationContext(),"Firebase Connected",Toast.LENGTH_LONG).show();
     }
 }
